da99f4f64a5fc7e35a44502e71b393df24b60a4b
Vojta <NapsikCzech@gmail.com>
2019-03-04 00:52:39 +0100

    Revert "Update"
    This reverts commit 1d5ceacfd9992cc241b76361756ab4411060cc80.

--- src/main/java/github/scarsz/discordsrv/util/LangUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/LangUtil.java
@@ -140,7 +140,7 @@ public class LangUtil {
                     "1. 서버의 {specialsourcefolder} 폴더로 들어갑니다.\n" +
                     "2. SpecialSource-1.7-SNAPSHOT.jar 파일을 삭제 합니다.\n" +
                     "3. SpecialSource v1.7.4를 http://central.maven.org/maven2/net/md-5/SpecialSource/1.7.4/SpecialSource-1.7.4.jar 에서 다운로드 받습니다.\n" +
-                    "4. {specialsourcefolder}로 3에서 다운로드 받은 파일을 복사합니다." +
+                    "4. {specialsourcefolder}로 3에서 다운로드 받은 파일을 복사합니다." + 
                     "5. 4에서 복사한 파일의 이름을 SpecialSource-1.7-SNAPSHOT.jar로 바꿉니다.\n" +
                     "6. 서버를 재부팅 합니다.\n" +
                     "\n" +
@@ -641,7 +641,7 @@ public class LangUtil {
             put(Language.ES, "No se encontro a nadie con Discord ID/nombre de Discord/nombre de Minecraft/Minecraft UUID \"{target}\" para buscar");
             put(Language.RU, "Никого с такими Discord ID/Discord ник/Minecraft ник/Minecraft UUID подходящего \"{target}\" не найдено.");
         }}), LINKED_ACCOUNT_REQUIRED(new HashMap<Language, String>() {{
-            put(Language.EN, "&cSkyLands &7&l⏐ &fYou attempted to say the following message to the game chat but this server requires that you have your Minecraft account linked to your Discord account. Link it in-game by typing `/discord link`.\n```{message}```");
+            put(Language.EN, "You attempted to say the following message to the game chat but this server requires that you have your Minecraft account linked to your Discord account. Link it in-game by typing `/discord link`.\n```{message}```");
             put(Language.FR, "Le message suivant n'a pas pu être envoyé sur le jeu car votre compte Minecraft doit être lié à votre compte Discord. Liez votre compte depuis Minecraft en tapant `/discord link`.\n```{message}```");
             put(Language.DE, "Du hast versucht die folgende Nachricht im Spielchat zu senden aber dieser Server verlangt, dass du deinen Minecraft-Account mit deinem Discord-Account verbinden musst. Verbinde sie, indem du im Spiel den Befehl `/discord link` eingibst.\n```{message}```");
             put(Language.JA, "ゲームチャットに以下のメッセージを表示しようとしましたが、このサーバーではあなたのMinecraftアカウントをDiscordアカウントにリンクさせる必要があります。リンクさせるには、ゲーム内で `/discord link` を実行してください。\n```{message}```");
@@ -650,7 +650,7 @@ public class LangUtil {
             put(Language.ES, "Intentaste decir el siguiente mensaje en el chat del juego, pero este servidor requiere que tenga su cuenta de Minecraft vinculada a su cuenta de Discord. Vinculalo en el juego escribiendo `/discord link`.\n```{message}```");
             put(Language.RU, "Вы попытались отправить сообщение в игровой чат из клиента Discord, однако сервер требует, чтобы вы привязали ваш Майнкрафт аккаунт к вашей учётной записи Discord. Чтобы связать эти аккаунты, используйте команду `/discord link` в игре.\n```{message}```");
         }}), ACCOUNT_ALREADY_LINKED(new HashMap<Language, String>() {{
-            put(Language.EN, "&cSkyLands &7&l⏐ &fYour Minecraft account is already associated with a Discord account. Should you have permission to, you can unlink your account with &c/discord unlink&f.");
+            put(Language.EN, "Your Minecraft account is already associated with a Discord account. Should you have permission to, you can unlink your account with /discord unlink.");
             put(Language.FR, "Votre compte Minecraft est déjà associé à un compte Discord. Si vous avez la permission vous pouvez utiliser la commande /discord unlink pour retirer votre compte Discord.");
             put(Language.DE, "Ihr Minecraft-Konto ist bereits mit einem Discord-Konto verknüpft. Sollten Sie die Erlaubnis haben, können Sie Ihre Konto-Verknüpfung mit /discord unlink aufheben.");
             put(Language.JA, "あなたのMinecraftアカウントはすでにDiscordアカウントに関連付けられています。 もしあなたがパーミッションを持っていれば、/discord unlinkを実行して2つのリンクを解除することができます。");
